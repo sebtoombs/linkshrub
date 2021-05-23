9679b016ab6e3fcab6c09530bf8b2e92c6a6bd5b
Seb <sebtoombs@gmail.com>
2021-05-23 17:08:22 +1000

    Add color picker input

---
+++ components/ColorPickerInput.tsx
@@ -0,0 +1,71 @@
+import { forwardRef, useState } from "react";
+import {
+  Popover,
+  PopoverTrigger,
+  Button,
+  PopoverContent,
+  Box,
+} from "@chakra-ui/react";
+import { ColorResult, RGBColor, SketchPicker } from "react-color";
+import { WithRestProps } from "../interfaces";
+
+export type ColorPickerInputProps = {
+  value: ColorPickerValue | undefined;
+  onChange: (value: ColorPickerValue) => void;
+};
+
+export type ColorPickerValue = string;
+
+function ColorPickerInput(props: ColorPickerInputProps, ref: any) {
+  const { onChange, value } = props;
+
+  const [selectedColor, setSelectedColor] = useState<string | undefined>(value);
+
+  const handleChange = (color: ColorResult) => {
+    const _value = color.hex;
+    setSelectedColor(_value);
+    if (typeof onChange === "function") {
+      onChange.call(null, _value);
+    }
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button pl="0">
+          <ColorSwatch
+            color={selectedColor}
+            mr="3"
+            roundedTopLeft="md"
+            roundedBottomLeft="md"
+          />
+          Pick color
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent width="220px" border="none">
+        <SketchPicker
+          color={selectedColor}
+          onChangeComplete={handleChange}
+          onChange={(color) => setSelectedColor(color.hex)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+export default forwardRef<any, ColorPickerInputProps>(ColorPickerInput);
+
+export type ColorSwatchProps = WithRestProps & {
+  color?: ColorPickerValue;
+};
+
+function ColorSwatch({ color, ...props }: ColorSwatchProps) {
+  return (
+    <Box
+      as="span"
+      width="3rem"
+      height="100%"
+      bg={color as unknown as string}
+      {...props}
+    />
+  );
+}
