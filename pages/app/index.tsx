c94231443a366edcf405f13eeb426f7d43b1d678
Seb <sebtoombs@gmail.com>
2021-05-23 13:49:28 +1000

    Update index.tsx
    REfactor page form into separate component, add query to fetch existing page

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -4,13 +4,15 @@ import {
   Box,
   useToast,
   VStack,
+  Input,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import AppLayout from "../../components/AppLayout";
 import Container from "../../components/Container";
 import FileUpload from "../../components/AvatarFileUpload";
 import Head from "../../components/Head";
 import ProtectedPage from "../../hoc/ProtectedPage";
-import { useMutation } from "urql";
+import { useMutation, useQuery } from "urql";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { ErrorMessage } from "@hookform/error-message";
@@ -18,6 +20,41 @@ import { useForm, Controller } from "react-hook-form";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/router";
 import gql from "graphql-tag";
+import { useUser } from "../../hooks/useUser";
+import { ImageField } from "../../interfaces";
+
+const FETCH_LINK_PAGE_QUERY = gql`
+  query FETCH_LINK_PAGE_QUERY($userID: ID!) {
+    allLinkPages(where: { owner: { id: $userID } }, first: 1) {
+      id
+      slug
+      title
+      heading
+      subheading
+      background
+      avatarImage {
+        id
+        width
+        height
+        src
+      }
+      links {
+        id
+        content
+        url
+        icon
+        iconColor
+        image {
+          id
+          width
+          height
+          src
+        }
+        imageBackground
+      }
+    }
+  }
+`;
 
 const UPDATE_PAGE_MUTATION = gql`
   mutation UPDATE_PAGE_MUTATION($id: ID!, $data: LinkPageUpdateInput) {
@@ -27,17 +64,57 @@ const UPDATE_PAGE_MUTATION = gql`
   }
 `;
 
+export type PageLinkFormData = {
+  content: string;
+  url: string;
+  icon: string;
+  iconColor: string;
+  image: File | ImageField;
+  imageBackground: string;
+};
+
 export type PageFormData = {
-  avatarImage: File;
+  avatarImage: File | ImageField;
+  title: string;
+  heading: string;
+  subheading: string;
+  slug: string;
+  background: string;
+  links: PageLinkFormData[];
 };
 
 const schema = yup.object().shape({});
 
 export function AppDashboardPage() {
-  const toast = useToast();
-  const router = useRouter();
-  const [updatePageResult, signin] = useMutation(UPDATE_PAGE_MUTATION);
+  const { user } = useUser();
 
+  const [fetchLinkPageResult, refetchLinkPage] = useQuery({
+    query: FETCH_LINK_PAGE_QUERY,
+    variables: { userID: user.id },
+  });
+
+  const { data, error, fetching } = fetchLinkPageResult;
+  const linkPage = useMemo(() => {
+    return data && data.allLinkPages?.length ? data.allLinkPages[0] : null;
+  }, [data]);
+
+  return (
+    <AppLayout>
+      <Head title="Dashboard" />
+      <Box>
+        <Container>
+          {!fetching && <LinkPageForm linkPage={linkPage} />}
+        </Container>
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default ProtectedPage(AppDashboardPage);
+
+function LinkPageForm({ linkPage }) {
+  const toast = useToast();
+  const [updatePageResult, updatePage] = useMutation(UPDATE_PAGE_MUTATION);
   const { data, error, fetching } = updatePageResult;
 
   useEffect(() => {
@@ -59,8 +136,14 @@ export function AppDashboardPage() {
   const formDefaults = useMemo<PageFormData>(() => {
     return {
       avatarImage: null,
+      title: linkPage?.title || "",
+      heading: linkPage?.heading || "",
+      subheading: linkPage?.subheading || "",
+      slug: linkPage?.slug,
+      background: linkPage?.background || "",
+      links: [],
     };
-  }, []);
+  }, [linkPage]);
 
   const { register, handleSubmit, formState, control } = useForm<PageFormData>({
     defaultValues: formDefaults,
@@ -68,33 +151,28 @@ export function AppDashboardPage() {
   });
 
   const onSubmitForm = handleSubmit(async (formValues) => {
-    const res = await signin(formValues);
-    if (res?.data?.authenticateUserWithPassword?.item) {
-      router.push("/app");
-    }
+    const res = await updatePage(formValues);
   });
 
   return (
-    <AppLayout>
-      <Head title="Dashboard" />
-      <Box>
-        <Container>
-          <form onSubmit={onSubmitForm} noValidate>
-            <VStack>
-              <FormControl id="avatarImage">
-                <FormLabel>Avatar</FormLabel>
-                <Controller
-                  name={`avatarImage`}
-                  control={control}
-                  render={({ field }) => <FileUpload {...field} />}
-                />
-              </FormControl>
-            </VStack>
-          </form>
-        </Container>
-      </Box>
-    </AppLayout>
+    <form onSubmit={onSubmitForm} noValidate>
+      <VStack spacing="5">
+        <FormControl id="avatarImage">
+          <FormLabel>Avatar</FormLabel>
+          <Controller
+            name={`avatarImage`}
+            control={control}
+            render={({ field }) => <FileUpload {...field} />}
+          />
+        </FormControl>
+        <FormControl id="heading" isInvalid={!!formState?.errors?.heading}>
+          <FormLabel>Heading</FormLabel>
+          <Input type="text" {...register("heading")} />
+          <FormErrorMessage>
+            <ErrorMessage errors={formState.errors} name="heading" />
+          </FormErrorMessage>
+        </FormControl>
+      </VStack>
+    </form>
   );
 }
-
-export default ProtectedPage(AppDashboardPage);
